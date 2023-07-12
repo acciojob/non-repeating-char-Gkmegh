249dd3af7658448be65b98328758e4291fe22c53
Gkmegh <118533859+Gkmegh@users.noreply.github.com>
2023-07-12 14:33:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+function solution (input) {
+    var obj = {};
+    for (let i = 0; i < input.length; i++) {
+        if (obj.hasOwnProperty(input[i])) {
+            obj[input[i]] += 1;
+        } else {
+            obj[input[i]] = 1;
+        }
+    }
+    for (let i = 0; i < input.length; i++) {
+        if (obj[input[i]] === 1) {
+            return input[i];
+        }
+    }
+}
